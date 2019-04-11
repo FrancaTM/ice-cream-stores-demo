e30fdccba0bd6245c75f04b777bc71c64dcc5c9d
Túlio <francatm@outlook.com>
2019-04-11 11:40:55 -0300

    Modify theme Clean code

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ice Creams FTW',
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          // primarySwatch: Colors.blue,
-          ),
+        primarySwatch: Colors.pink,
+        scaffoldBackgroundColor: Colors.pink[50],
+      ),
       home: const HomePage(title: 'Ice Cream Stores in SF'),
     );
   }
@@ -44,19 +36,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
